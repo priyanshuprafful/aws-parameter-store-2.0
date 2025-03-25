3d8df834782108a3179309799dd0ed5950c31e5b
priyanshuprafful <priyanshuprafful@gmail.com>
2025-03-26 03:14:13 +0530

    updating ./aws-parameter-store-2.0

---
+++ main.tf
@@ -0,0 +1,16 @@
+resource "aws_ssm_parameter" "parameters_store" {
+  count = length(var.parameters)
+  name = var.parameters[count.index].name
+  type = var.parameters[count.index].type
+  value = var.parameters[count.index].value
+
+}
+
+variable "parameters" {
+  default = [
+    {name = "demo1", value = "demo1" , type = "String"},
+    {name = "demo2", value = "demo2" , type = "String"},
+    {name = "demo3", value = "demo3" , type = "String"},
+    {name = "demo4", value = "demo4" , type = "String"},
+  ]
+} 
