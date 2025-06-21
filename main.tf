4612c814d0e199bc19af43969ad0d0a1721df1ae
priyanshuprafful <priyanshuprafful@gmail.com>
2025-06-21 16:02:29 +0530

    updating ./aws-parameter-store-2.0

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ resource "aws_ssm_parameter" "parameters_store" {
 
 variable "parameters" {
   default = [
-    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.saraldevops.site" , type = "String"},
-    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.saraldevops.site" , type = "String"},
-    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.saraldevops.site" , type = "String"},
-    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.saraldevops.site" , type = "String"},
-    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.saraldevops.site" , type = "String"},
+    {name = "roboshop.dev.frontend.catalogue_url", value = "http://catalogue-dev.devopspro.fun" , type = "String"},
+    {name = "roboshop.dev.frontend.user_url", value = "http://user-dev.devopspro.fun" , type = "String"},
+    {name = "roboshop.dev.frontend.cart_url", value = "http://cart-dev.devopspro.fun" , type = "String"},
+    {name = "roboshop.dev.frontend.payment_url", value = "http://payment-dev.devopspro.fun" , type = "String"},
+    {name = "roboshop.dev.frontend.shipping_url", value = "http://shipping-dev.devopspro.fun" , type = "String"},
 
   ]
 }
