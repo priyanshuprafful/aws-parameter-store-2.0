9a95054abbb090d1b9f817cfcf87e6a43a748f9b
priyanshuprafful <priyanshuprafful@gmail.com>
2025-08-05 17:00:20 +0530

    updating ./aws-parameter-store-2.0

--- main.tf
+++ main.tf
@@ -1,16 +1,30 @@
-resource "aws_ssm_parameter" "param" {
-
-  count = length(var.params)
-  name = var.params[count.index].name
-  type = var.params[count.index].type
-  value = var.params[count.index].value
+# resource "aws_ssm_parameter" "param" {
+#
+#   count = length(var.params)
+#   name = var.params[count.index].name
+#   type = var.params[count.index].type
+#   value = var.params[count.index].value
+# }
+#
+# variable "params" {
+#   default = [
+#     { name =  "demo1", value = "demo_value", type = "String" },
+#     { name =  "demo2", value = "demo_value2", type = "String" },
+#     { name =  "demo3", value = "demo_value3", type = "String" }
+#
+#   ]
+# }
+variable "params_map" {
+  default = {
+    demo1 = { value = "demo_value", type = "String" },
+    demo2 = { value = "demo_value2", type = "String" },
+    demo3 = { value = "demo_value3", type = "String" }
+  }
 }
 
-variable "params" {
-  default = [
-    { name =  "demo1", value = "demo_value", type = "String" },
-    { name =  "demo2", value = "demo_value2", type = "String" },
-    { name =  "demo3", value = "demo_value3", type = "String" }
-
-  ]
+resource "aws_ssm_parameter" "param" {
+  for_each = var.params_map
+  name  = each.key
+  value = each.value.value
+  type  = each.value.type
 }
