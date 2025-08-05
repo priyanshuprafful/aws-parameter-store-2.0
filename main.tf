c697c8343b7434b2c0871ec633ad608bcf22bdaf
priyanshuprafful <priyanshuprafful@gmail.com>
2025-08-05 17:40:07 +0530

    updating ./aws-parameter-store-2.0

--- main.tf
+++ main.tf
@@ -16,11 +16,11 @@
 # }
 variable "params_map" {
   default = {
-    roboshop.dev.frontend.catalogue_url = { value = "http://catalogue-dev.devopspro.fun", type = "String" },
-    roboshop.dev.frontend.user_url = { value = "http://user-dev.devopspro.fun", type = "String" },
-    roboshop.dev.frontend.cart_url = { value = "http://cart-dev.devopspro.fun", type = "String" },
-    roboshop.dev.frontend.payment_url = { value = "http://payment-dev.devopspro.fun", type = "String" },
-    roboshop.dev.frontend.shipping_url = { value = "http://shipping-dev.devopspro.fun", type = "String" },
+    "roboshop.dev.frontend.catalogue_url" = { value = "http://catalogue-dev.devopspro.fun", type = "String" },
+    "roboshop.dev.frontend.user_url" = { value = "http://user-dev.devopspro.fun", type = "String" },
+    "roboshop.dev.frontend.cart_url" = { value = "http://cart-dev.devopspro.fun", type = "String" },
+    "roboshop.dev.frontend.payment_url" = { value = "http://payment-dev.devopspro.fun", type = "String" },
+    "roboshop.dev.frontend.shipping_url" = { value = "http://shipping-dev.devopspro.fun", type = "String" },
 
 
 
